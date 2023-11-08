5192f54da10868632264a608bece663ebccaf998
Samuel Hurtado Marin <main@samuelhurtado.com>
2023-11-08 09:20:37 +0100

    Estructura vistas SuperAdmin + Controller (empty)

---
+++ Controllers/SuperAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LostArkOffice.Controllers
+{
+    public class SuperAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
